8dc000e7bc237bc05ae51d7d521760ceb5410522
Conor O'Brien <obriencjj@gmail.com>
2025-08-01 02:58:34 +0100

    Using nightly build internal package manager!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,25 @@ print("hi")
 require("autocommands")
 require("keymaps")
 require("options")
+print("lol2")
+
+
+vim.pack.add({
+    "https://github.com/stevearc/oil.nvim",
+    { src = "https://github.com/neovim/nvim-lspconfig" }
+})
+
+
+vim.lsp.enable({
+    "lua_ls",
+})
+
+
+require("oil").setup({
+    columns = {}, -- Disable icons.
+    view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+    },
+})
+vim.keymap.set("n", "<leader>e", ":Oil<cr>", { desc = "Open Oil" })
