2f827a2b2537d9b0e813a8011dbb1bc2c3e4f73d
Conor O'Brien <obriencjj@gmail.com>
2025-08-01 09:45:27 +0100

    Yank autocommand.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,9 +1,16 @@
--- Autocommands for formatting on save
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 vim.api.nvim_create_autocmd("BufWritePre", {
     group = augroup,
     callback = function()
         vim.lsp.buf.format({ async = true })
     end,
+    desc = "Format on save",
+    group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.hl.on_yank({ higroup = "Yank", timeout = 150 })
+    end,
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
 })
