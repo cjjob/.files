de07140e2d07382d48b04810c3546e4498b95714
Conor O'Brien <obriencjj@gmail.com>
2025-08-01 02:59:23 +0100

    Basic format on save autocommand.

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,1 +1,9 @@
-print("hi from autocommands")
+-- Autocommands for formatting on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup,
+    callback = function()
+        vim.lsp.buf.format({ async = true })
+    end,
+})
