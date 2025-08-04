48dd1d65609476f6de2f4ac9bac0284d5a893794
Conor O'Brien <obriencjj@gmail.com>
2025-08-04 16:32:01 +0100

    More attempts to get centering working everywhere.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -5,6 +5,29 @@ km.set("n", "<left>", "<left>zz")
 km.set("n", "<right>", "<right>zz")
 km.set("n", "<up>", "<up>zz")
 km.set("n", "<down>", "<down>zz")
+km.set("n", "dd", "ddzz")
+km.set("n", "G", "Gzz")
+-- These commands drop you into insert mode and so you can't just type "zz"
+-- because it would just write text. You need to enter Insert-Normal mode (i.e.
+-- be able to fire off a single Normal mode command) with Ctrl-o and then
+-- execute zz, before automatically returing to Insert mode.
+km.set("n", "o", "o<C-o>zz")
+km.set("n", "O", "O<C-o>zz")
+-- Similar to the above, except you're already in insert mode.
+km.set("i", "<left>", "<left><C-o>zz")
+km.set("i", "<right>", "<right><C-o>zz")
+km.set("i", "<up>", "<up><C-o>zz")
+km.set("i", "<down>", "<down><C-o>zz")
+km.set("i", "<CR>", "<CR><C-o>zz")
+-- And for every character. This is dumb af.
+-- TODO: This isn't working. Maybe just switch to the plugin the guy made.
+local chars = {}
+for i = string.byte("a"), string.byte("z") do
+    chars[#chars + 1] = string.char(i)
+end
+for _, char in ipairs(chars) do
+    km.set("i", char, char .. "<C-o>zz")
+end
 
 km.set("n", "<CR>", "o<Esc>zz", { desc = "Add new line. Stay normal." })
 km.set("n", "<S-CR>", "O<Esc>zz", { desc = "Add new line above. Stay normal." })
